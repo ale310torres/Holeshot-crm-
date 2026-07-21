2ccfcfaa0cbf6f97c6dc2656cca2a6044a45e7a3
ale310torres <ale310torres@gmail.com>
2026-07-20 22:14:06 -0400

    Add direct lead save v15

---
+++ holeshot-lead-save-v15.js
@@ -0,0 +1,5 @@
+(function () {
+  const SUPABASE_URL = "https://esjqybxzpqtonkcomdtb.supabase.co";
+  const SUPABASE_ANON_KEY =
+    "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXAiYmFzZSIsInJlZiI6ImVzanF5Ynh6cHF0b25rY29tZHRiIiwicm9sZSI6ImFub24iLCJpYXQiOjE3ODQxNTE2NTgsImV4cCI6MjA5OTcyNzY1OH0.DEGP5xyOsdl41qFiFaoZfophSKiWdXIcKmq7_wv9nbg";
+})();
